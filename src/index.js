c4e37648e5b355b551ae7ab13bb3708c752df2e6
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-02-18 14:03:45 +0000

    fix eslint

--- src/index.js
+++ src/index.js
@@ -8,27 +8,27 @@ class TaskObj {
   }
 }
 
-//Code Action
+//  Code Action
 
-let task1 = new TaskObj('Wash the dishes', true, 0);
-let task2 = new TaskObj('Complete to do list', true, 1);
+const task1 = new TaskObj('Wash the dishes', true, 0);
+const task2 = new TaskObj('Complete to do list', true, 1);
 const taskArr = [];
 taskArr.push(task1, task2);
 
 const display = (item, index) => {
-  //Variables
+  //  Variables
 
-  let taskIndex = taskArr[index].index;
+  const taskIndex = taskArr[index].index;
   const taskSection = document.querySelector('#taskSection');
   const div = document.createElement('div');
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
   const span = document.createElement('span');
-  span.innerHTML = `&#8942;`;
+  span.innerHTML = '&#8942;';
   span.classList.add('symbols');
   const taskText = document.createElement('span');
 
-  //Display Actions
+  //  Display Actions
 
   taskText.innerHTML = taskArr[taskIndex].description;
   div.appendChild(checkbox);
